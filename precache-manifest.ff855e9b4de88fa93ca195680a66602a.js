ad9c17039b9e66ae60ef4c82a9430270d9b12225
Zach <Zacaframe@gmail.com>
2020-07-27 21:18:08 -0400

    Updates

---
+++ precache-manifest.ff855e9b4de88fa93ca195680a66602a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "364b3140b8cb3529dd0fde82dc2b8e82",
+    "url": "/web-design-challenge/index.html"
+  },
+  {
+    "revision": "1ae95313ac14bfc9b195",
+    "url": "/web-design-challenge/static/css/main.dbad03ee.chunk.css"
+  },
+  {
+    "revision": "91d3f8bacfd62cd2f0c7",
+    "url": "/web-design-challenge/static/js/2.a3116365.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/web-design-challenge/static/js/2.a3116365.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ae95313ac14bfc9b195",
+    "url": "/web-design-challenge/static/js/main.f30e5ba7.chunk.js"
+  },
+  {
+    "revision": "bde90a2a26807d7b2479",
+    "url": "/web-design-challenge/static/js/runtime-main.2152ce84.js"
+  },
+  {
+    "revision": "4c464d8d12f8ce8868aab8fa95f45716",
+    "url": "/web-design-challenge/static/media/fig1.4c464d8d.png"
+  },
+  {
+    "revision": "bca2d243cab23ea34656cf67621c8b41",
+    "url": "/web-design-challenge/static/media/fig2.bca2d243.png"
+  },
+  {
+    "revision": "5f4de63a8c5355006d7cbc26854a4612",
+    "url": "/web-design-challenge/static/media/fig3.5f4de63a.png"
+  },
+  {
+    "revision": "376197c6ce0fec88fc2bd68c11465bc7",
+    "url": "/web-design-challenge/static/media/fig4.376197c6.png"
+  }
+]);
